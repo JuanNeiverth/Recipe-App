19aa1589fabfbd605e2026db21ec3eaa7ce556ab
Juan marcos Polli neiverth <polli.neiverth@gmail.com>
2023-05-17 17:41:02 -0500

    Initial Commit

---
+++ Challenger MVVM Prt 2/Challenger_MVVM_Prt_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenger_MVVM_Prt_2App.swift
+//  Challenger MVVM Prt 2
+//
+//  Created by Juan on 17/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Challenger_MVVM_Prt_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
